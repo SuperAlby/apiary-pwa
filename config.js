b488f8d9521107818d4e6be7223935822b6ebacf
SuperAlby <74138414+SuperAlby@users.noreply.github.com>
2025-09-18 15:30:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://enfhboxodoffavmboqlo.supabase.co"; // <-- incolla qui
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuZmhib3hvZG9mZmF2bWJvcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMTQ3MDIsImV4cCI6MjA3MzY5MDcwMn0.ptRD2kyBaa3zStw4Q7TBvQ3ce9oaFndMOJcX3XwQoOA"; // <-- incolla qui
-export const APP_NAME = "Apiary Manager";
+export const APP_NAME = "ApisPlanet";
